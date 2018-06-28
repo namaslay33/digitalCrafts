279c8adf67b11eff8d205cb67d0e0fabaf4a26a9
Skylar Hunt <SkylarHunt@skylarhsmacbook.localdomain>
2018-06-28 11:42:14 -0500

    Uncommented Python 101

--- python-exercises/W1/Wed/Python 101 Exercises 4-10.py
+++ python-exercises/W1/Wed/Python 101 Exercises 4-10.py
@@ -1,56 +1,56 @@
 #Question 4 - Day of the Week
-# print("Please choose a number for the day. Sunday = 0, Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6")
-# day = input("What is the day?")
+print("Please choose a number for the day. Sunday = 0, Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6")
+day = input("What is the day?")
 
-# dayOfWeek = int(day)
-# if (dayOfWeek == 0):
-#     print("Sunday")
-# elif (dayOfWeek == 1):
-#     print("Monday")
-# elif (dayOfWeek == 2):
-#     print("Tuesday")
-# elif (dayOfWeek == 3):
-#     print("Wednesday")
-# elif (dayOfWeek == 4):
-#     print("Thursday")
-# elif (dayOfWeek == 5):
-#     print("Friday")
-# else:
-#     print("Saturday")
+dayOfWeek = int(day)
+if (dayOfWeek == 0):
+    print("Sunday")
+elif (dayOfWeek == 1):
+    print("Monday")
+elif (dayOfWeek == 2):
+    print("Tuesday")
+elif (dayOfWeek == 3):
+    print("Wednesday")
+elif (dayOfWeek == 4):
+    print("Thursday")
+elif (dayOfWeek == 5):
+    print("Friday")
+else:
+    print("Saturday")
 
-#Question 5 - Work or Sleep In
-# day = int(input("What is the day?"))
+Question 5 - Work or Sleep In
+day = int(input("What is the day?"))
 
-# if (day > 0 and day < 6):
-#     print("Go to Work")
-# else:
-#     print("Sleep In... zzz")
+if (day > 0 and day < 6):
+    print("Go to Work")
+else:
+    print("Sleep In... zzz")
 
-# #Question 6 - Celsius to Fahrenheit
-# tempCelsius = input("What is the temperature in degrees Celsius?")
-# tempFahrenheit = int(tempCelsius) * 1.8 + 32.0
+#Question 6 - Celsius to Fahrenheit
+tempCelsius = input("What is the temperature in degrees Celsius?")
+tempFahrenheit = int(tempCelsius) * 1.8 + 32.0
 
-# print("This is equivalent to " + str(tempFahrenheit) + " F.")
+print("This is equivalent to " + str(tempFahrenheit) + " F.")
 
-#Question 7 - Tip Calculator
-# bill = int(input("What is the total bill?"))
-# service = input("How would you rate the level of service? Good, fair, or bad?")
+Question 7 - Tip Calculator
+bill = int(input("What is the total bill?"))
+service = input("How would you rate the level of service? Good, fair, or bad?")
 
-# goodSer = .2 * bill
-# fairSer = .15 * bill
-# badSer = .1 * bill
+goodSer = .2 * bill
+fairSer = .15 * bill
+badSer = .1 * bill
 
-# if (service == "good"):
-#     bill = goodSer + bill
-#     print("Tip Amount: $" + '{:,.2f}'.format(goodSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
-# elif (service == "fair"):
-#     bill = fairSer + bill
-#     print("Tip Amount: $" + '{:,.2f}'.format(fairSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
-# else:
-#     bill = badSer + bill
-#     print("Tip Amount: $" + '{:,.2f}'.format(badSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
+if (service == "good"):
+    bill = goodSer + bill
+    print("Tip Amount: $" + '{:,.2f}'.format(goodSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
+elif (service == "fair"):
+    bill = fairSer + bill
+    print("Tip Amount: $" + '{:,.2f}'.format(fairSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
+else:
+    bill = badSer + bill
+    print("Tip Amount: $" + '{:,.2f}'.format(badSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
 
-#Question 8 - Tip Calculator 2
+Question 8 - Tip Calculator 2
 bill = int(input("What is the total bill?"))
 service = input("How would you rate the level of service? Good, fair, or bad?")
 split = int(input("Split how many ways?"))
@@ -72,23 +72,23 @@ else:
     billSplit = bill / split
     print("Tip Amount: $" + '{:,.2f}'.format(badSer) + " Total Bill: $" + '{:,.2f}'.format(bill) + " Amount per person: $" + '{:,.2f}'.format(billSplit))
 
-#Question 9 - Print 1 to 10
-# count = 0
-# while count < 10:
-#     count += 1
-#     print(count)
+Question 9 - Print 1 to 10
+count = 0
+while count < 10:
+    count += 1
+    print(count)
 
-#Question 10 - How Many Coins
-# print("You have 0 coins.")
-# reply = input("Do you want another?")
-# coins = 0
+Question 10 - How Many Coins
+print("You have 0 coins.")
+reply = input("Do you want another?")
+coins = 0
 
-# if (reply == "Yes"):
-#     coins += 1
-#     print("Yes. You have " + str(coins) + " coins.") 
-#     reply = input("Do you want another?")
-# else:
-#     print("No? Bye")
+if (reply == "Yes"):
+    coins += 1
+    print("Yes. You have " + str(coins) + " coins.") 
+    reply = input("Do you want another?")
+else:
+    print("No? Bye")
     
 
 
