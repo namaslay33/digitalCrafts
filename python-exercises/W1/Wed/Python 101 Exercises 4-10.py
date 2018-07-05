de378878b34f9cd1a58a3f6449b48963064072b7
Skylar Hunt <SkylarHunt@skylar-hunts-macbook.local>
2018-07-05 15:46:52 -0500

    Added Python 101 Exercise 4-10

--- python-exercises/W1/Wed/Python 101 Exercises 4-10.py
+++ python-exercises/W1/Wed/Python 101 Exercises 4-10.py
@@ -1,6 +1,6 @@
 #Question 4 - Day of the Week
 print("Please choose a number for the day. Sunday = 0, Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6")
-day = input("What is the day?")
+day = input("What is the day? ")
 
 dayOfWeek = int(day)
 if (dayOfWeek == 0):
@@ -18,8 +18,8 @@ elif (dayOfWeek == 5):
 else:
     print("Saturday")
 
-Question 5 - Work or Sleep In
-day = int(input("What is the day?"))
+# Question 5 - Work or Sleep In
+day = int(input("What is the day? "))
 
 if (day > 0 and day < 6):
     print("Go to Work")
@@ -32,65 +32,62 @@ tempFahrenheit = int(tempCelsius) * 1.8 + 32.0
 
 print("This is equivalent to " + str(tempFahrenheit) + " F.")
 
-Question 7 - Tip Calculator
-bill = int(input("What is the total bill?"))
-service = input("How would you rate the level of service? Good, fair, or bad?")
+# Question 7 - Tip Calculator
+bill = int(input("What is the total bill? "))
+service = input("How would you rate the level of service? Good, fair, or bad? ")
 
 goodSer = .2 * bill
 fairSer = .15 * bill
 badSer = .1 * bill
 
 if (service == "good"):
-    bill = goodSer + bill
+    bill += goodSer
     print("Tip Amount: $" + '{:,.2f}'.format(goodSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
 elif (service == "fair"):
-    bill = fairSer + bill
+    bill += fairSer
     print("Tip Amount: $" + '{:,.2f}'.format(fairSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
 else:
-    bill = badSer + bill
+    bill += badSer
     print("Tip Amount: $" + '{:,.2f}'.format(badSer) + " Total Bill: $" + '{:,.2f}'.format(bill))
 
-Question 8 - Tip Calculator 2
-bill = int(input("What is the total bill?"))
-service = input("How would you rate the level of service? Good, fair, or bad?")
-split = int(input("Split how many ways?"))
+# Question 8 - Tip Calculator 2
+bill = int(input("What is the total bill? "))
+service = input("How would you rate the level of service? Good, fair, or bad? ")
+split = int(input("Split how many ways? "))
 
 goodSer = .2 * bill
 fairSer = .15 * bill
 badSer = .1 * bill
 
 if (service == "good"):
-    bill = goodSer + bill
+    bill += goodSer
     billSplit = bill / split
     print("Tip Amount: $" + '{:,.2f}'.format(goodSer) + " Total Bill: $" + '{:,.2f}'.format(bill) + " Amount per person: $" + '{:,.2f}'.format(billSplit))
 elif (service == "fair"):
-    bill = fairSer + bill
+    bill += fairSer
     billSplit = bill / split
     print("Tip Amount: $" + '{:,.2f}'.format(fairSer) + " Total Bill: $" + '{:,.2f}'.format(bill) + " Amount per person: $" + '{:,.2f}'.format(billSplit))
 else:
-    bill = badSer + bill
+    bill += badSer
     billSplit = bill / split
     print("Tip Amount: $" + '{:,.2f}'.format(badSer) + " Total Bill: $" + '{:,.2f}'.format(bill) + " Amount per person: $" + '{:,.2f}'.format(billSplit))
 
-Question 9 - Print 1 to 10
+# # Question 9 - Print 1 to 10
 count = 0
 while count < 10:
     count += 1
     print(count)
 
-Question 10 - How Many Coins
+
+# # Question 10 - How Many Coins
 print("You have 0 coins.")
-reply = input("Do you want another?")
+reply = input("Do you want a coin? ")
 coins = 0
 
-if (reply == "Yes"):
-    coins += 1
-    print("Yes. You have " + str(coins) + " coins.") 
-    reply = input("Do you want another?")
-else:
-    print("No? Bye")
+while reply == "Yes":
+    if (reply == "Yes"):
+        coins += 1
+        print("Yes. You have " + str(coins) + " coins.") 
+        reply = input("Do you want another?")
+print("No? Bye")
     
-
-
-
-
