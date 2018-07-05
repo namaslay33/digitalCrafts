f25e18576525b07b760210426222a0d0f479dcd7
Skylar Hunt <SkylarHunt@skylar-hunts-macbook.local>
2018-07-05 16:17:16 -0500

    Submitted String Exercises

--- python-exercises/W1/Wed/StringExercises.py
+++ python-exercises/W1/Wed/StringExercises.py
@@ -1,18 +1,19 @@
-# #1 Uppercase a String
+#1 Uppercase a String
 theCarters = "the carters is the name of new the beyonce and jayz cd"
 print(theCarters.upper())
 
-# #2 Capitalize a String
-print(theCarters.capitalize())
+#2 Capitalize a String
+print(theCarters.capitalize())                      #Capitalizes 1 word
+print(theCarters.title())                           #Capitalizes every word in the string
 
-# #3 Reverse a String
+#3 Reverse a String
 drake = "That's a real one in your reflection"
-drake = drake[::-1] #Note sky: Used the splice method here
+drake = drake[::-1] #Note: Used the splice method here
 print(drake)
 
-# #4 Leetspeak
-lion = "The Circle of Life"
-lion = lion.upper()
+#4 Leetspeak
+leetspeak = input("Enter in a phrase: ")
+leetspeak = leetspeak.upper()
 
 strA = "A"
 strE = "E"
@@ -22,19 +23,19 @@ strO = "O"
 strS = "S"
 strT = "T"
 
-if lion.find(strA) >= 0 :
-    lion = lion.replace(strA, "4")
-if lion.find(strE) >= 0:
-    lion = lion.replace(strE, "3")
-if lion.find(strG) >= 0 : 
-    lion = lion.replace(strG, "6")
-if lion.find(strI) >= 0 :
-    lion = lion.replace(strI, "1")
-if lion.find(strO) >= 0 :
-    lion = lion.replace(strS, "5")
-if lion.find(strT) == 0 :
-    lion = lion.replace(strT, "7")
-print(lion)
+if leetspeak.find(strA) >= 0 :
+    leetspeak = leetspeak.replace(strA, "4")
+if leetspeak.find(strE) >= 0:
+    leetspeak = leetspeak.replace(strE, "3")
+if leetspeak.find(strG) >= 0 : 
+    leetspeak = leetspeak.replace(strG, "6")
+if leetspeak.find(strI) >= 0 :
+    leetspeak = leetspeak.replace(strI, "1")
+if leetspeak.find(strO) >= 0 :
+    leetspeak = leetspeak.replace(strS, "5")
+if leetspeak.find(strT) == 0 :
+    leetspeak = leetspeak.replace(strT, "7")
+print(leetspeak)
 
 #5 Long Long Vowel 
 words = "I am Supreme"
@@ -56,12 +57,12 @@ if words.find(vowU) >= 0 :
     words = words.replace(vowU, "uuuuu")
 print(words)
 
-#6 Caesar Cipher
+# #6 Caesar Cipher
 cipher = "lbh zhfg hayrnea jung lbh unir yrnearq"
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-print(alphabet)
+# print(alphabet)
 position = 0
 newPosition = 0
 finalString = ""
