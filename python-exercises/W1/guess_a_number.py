55d907615431ad981ea468bbf3515a116acc5853
Skylar Hunt <SkylarHunt@skylar-hunts-macbook.local>
2018-07-08 06:55:03 -0500

    uploaded guess a number

--- python-exercises/W1/guess_a_number.py
+++ python-exercises/W1/guess_a_number.py
@@ -16,17 +16,17 @@
 # print("Yes, you win!")
     
 
-Step 2 - Give High-Low Hint
-while number != 5:
-    if number > 5:
-        print("Nope, too high. Try again")
-        number = input("What's the number?")
-    elif number < 5:
-        print("Nope, too low. Try again")
-        number = input("What's the number?")
-    else:
-        break
-print("Yes, you win!")
+#Step 2 - Give High-Low Hint
+# while number != 5:
+#     if number > 5:
+#         print("Nope, too high. Try again")
+#         number = input("What's the number?")
+#     elif number < 5:
+#         print("Nope, too low. Try again")
+#         number = input("What's the number?")
+#     else:
+#         break
+# print("Yes, you win!")
 
 # Step 3 - Randomly Generated Secret Number
 
@@ -46,4 +46,62 @@ print("Yes, you win!")
 #         break
 # print("Yes, you win!")
 
+#Step 4 - Limit Number of Guesses
+
+# import random
+# attempts = 0
+
+# print("This is the random number game. You have 5 chances to guess corectly. May the odds be in your favor.")
+
+# my_random_number = random.randint(1, 10)
+# while attempts < 5:
+    
+#     randoNumber = int(input("What is the secretly generated number? \n "))
+
+#     attempts += 1
+#     if randoNumber < my_random_number:
+#         print("Nope, too low. Try again. ")
+#     if randoNumber > my_random_number:
+#         print("Nope too high. Try again. ")
+#     if randoNumber == my_random_number:
+#         break
+#     if attempts < 5:
+#         print("You have " + str(5 - attempts) + " guesses left. ") 
+# if randoNumber == my_random_number:
+#     print("Yes, you win. ")
+# else:
+#     print("You ran out of guesses. The correct value was " + str(my_random_number) + ". ")
+
+#Step 5
+
+import random
+attempts = 0
+
+print("This is the random number game. You have 5 chances to guess corectly. May the odds be in your favor.")
+
+my_random_number = random.randint(1, 10)
+while attempts < 5:
+    
+    randoNumber = int(input("What is the secretly generated number? \n "))
+
+    attempts += 1
+    if randoNumber < my_random_number:
+        print("Nope, too low. Try again. ")
+    if randoNumber > my_random_number:
+        print("Nope too high. Try again. ")
+    if randoNumber == my_random_number:
+        break
+    if attempts < 5:
+        print("You have " + str(5 - attempts) + " guesses left. ") 
+if randoNumber == my_random_number:
+    print("Yes, you win. ")
+    
+    playAgain = input("Do you want to play again? (Y or N) ")
+
+    if playAgain == "N":
+        print("Bye. ")
+    else:
+        print("Rerun game. ") #or some code to get the game to restart.
+else:
+    print("You ran out of guesses. The correct value was " + str(my_random_number) + ". ")
     
