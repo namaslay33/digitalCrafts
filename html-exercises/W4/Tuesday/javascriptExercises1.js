e92424fe5fb0c54b998ad919ea62c8214dd5ef1f
Skylar Hunt <SkylarHunt@skylarhsmacbook.localdomain>
2018-07-17 14:29:24 -0500

    Changed file type from html to js

---
+++ html-exercises/W4/Tuesday/javascriptExercises1.js
@@ -0,0 +1,27 @@
+
+//Javascript Exercise 1
+
+//1) Declare a variable w/o Initializing It
+var name;
+
+//2) Declare and instantiate a number and return it 
+var num = 1;
+
+//3) Declare and instantiate a floating point number that isn't a whole number and return it
+var ice = 8.4;
+
+//4) Declare and instantiate a string "Hello World!" and return it
+var string = `Hello World!`;
+
+//5)Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+var myArray = [`Hello World!`, 4];
+
+//6) Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+var myObject = {
+    string = `Hello World!`,
+    number = 4
+}
+
+//7) Declare and instantiate a boolean value 'false' and return it
+var reply = false;
+
