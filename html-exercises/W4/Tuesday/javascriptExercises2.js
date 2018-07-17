1eaed2021e0449afd58f656209a7d2532f36e7e9
Skylar Hunt <SkylarHunt@skylarhsmacbook.localdomain>
2018-07-17 14:29:56 -0500

    fixed a few things

--- html-exercises/W4/Tuesday/javascriptExercises2.js
+++ html-exercises/W4/Tuesday/javascriptExercises2.js
@@ -3,7 +3,7 @@
 // 1 Return the sum of num1 and num2
 function sum(num1, num2) {
 
-    console.log("1) " + (num1 + num2))
+    console.log("1) " + (num1 + num2));
 
 }
 
@@ -11,7 +11,7 @@ sum(10, 4)
 
 //2 Return the difference of num1 and num2
 function diff(num1, num2) {
-	console.log("2) " + (num1 - num2))
+	console.log("2) " + (num1 - num2));
 
 }
 
@@ -19,14 +19,14 @@ diff(10,4)
 
 //3) Return the multiplication of num1 and num 2
 function mult(num1, num2) {
-	console.log("3) " + num1 * num2)
+	console.log("3) " + num1 * num2);
 }
 
 mult(5,8)
 
 // 4) Return the division of num1 and num2
 function div(num1, num2) {
-	console.log("4) " + (num1 / num2))
+	console.log("4) " + (num1 / num2));
 
 }
 
@@ -35,7 +35,7 @@ div(25, 5)
 // 5) Add 2 to num1 using += and return it
 function num1(num1) {
     num1 +=2
-	console.log("5) " + num1)
+	console.log("5) " + num1);
 	
 }
 num1(98)
