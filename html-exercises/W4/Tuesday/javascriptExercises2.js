447a9144dcfcade2a82b03719a1689d9325d3d49
Skylar Hunt <SkylarHunt@skylarhsmacbook.localdomain>
2018-07-17 12:16:40 -0500

    Uploaded Javascript Exercises 2

---
+++ html-exercises/W4/Tuesday/javascriptExercises2.js
@@ -0,0 +1,41 @@
+//Javascript Exercises 2
+
+// 1 Return the sum of num1 and num2
+function sum(num1, num2) {
+
+    console.log("1) " + (num1 + num2))
+
+}
+
+sum(10, 4)
+
+//2 Return the difference of num1 and num2
+function diff(num1, num2) {
+	console.log("2) " + (num1 - num2))
+
+}
+
+diff(10,4)
+
+//3) Return the multiplication of num1 and num 2
+function mult(num1, num2) {
+	console.log("3) " + num1 * num2)
+}
+
+mult(5,8)
+
+// 4) Return the division of num1 and num2
+function div(num1, num2) {
+	console.log("4) " + (num1 / num2))
+
+}
+
+div(25, 5)
+
+// 5) Add 2 to num1 using += and return it
+function num1(num1) {
+    num1 +=2
+	console.log("5) " + num1)
+	
+}
+num1(98)
