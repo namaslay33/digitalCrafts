4c3a654434dd559406fe51e2a9276d2826370d61
Skylar Hunt <SkylarHunt@skylarhsmacbook.localdomain>
2018-07-17 13:48:00 -0500

    Uploaded Exercise 4

---
+++ html-exercises/W4/Tuesday/javascriptExercises4.js
@@ -0,0 +1,43 @@
+// Javascript Exercises 4
+
+//1) Return the boolean value "true"
+function value() {
+    var yes = true
+    console.log("1) " + yes)	
+}
+value()
+
+//2 Return the boolean value "false"
+function value2() {
+    var no = false
+    console.log("2) " + no)
+
+}
+value2()
+
+//3 Return the opposite of the input boolean value
+function lvl4exercise3(bool) {
+	var yes = true
+    console.log("3) " + !yes)
+}
+lvl4exercise3()
+
+//4 Return the logical "and" of the input boolean values
+function lvl4exercise4(bool1, bool2) {
+	console.log("4) " + bool1 + " && " + bool2)
+
+}
+lvl4exercise4(true, false)
+
+//5 Return the logical "or" of the input boolean values
+function lvl4exercise5(bool1, bool2) {
+	console.log("5) " + bool1 + " || " + bool2)
+	
+}
+lvl4exercise5(true, false)
+
+//6 Return the logical "equivalence" of the input boolean values
+function lvl4exercise6(bool1, bool2) {
+	console.log("6) " + bool1 + " == " + bool2)
+}
+lvl4exercise6(true, true)
